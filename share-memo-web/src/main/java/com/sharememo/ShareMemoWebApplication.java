fc82b5388cbbc6c8a5fe561ba5b55aae090753bb
Jason <ya850805@gmail.com>
2021-12-23 23:13:44 +0800

    feat: Add web module and test controller.

---
+++ share-memo-web/src/main/java/com/sharememo/ShareMemoWebApplication.java
@@ -0,0 +1,12 @@
+package com.sharememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMemoWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShareMemoWebApplication.class, args);
+  }
+}
---
+++ share-memo-web/src/test/java/com/sharememo/ShareMemoWebApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sharememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMemoWebApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
