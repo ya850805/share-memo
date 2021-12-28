9739e494fdd5d4cf07891de20029f555aa0cb7d1
Jason <ya850805@gmail.com>
2021-12-28 21:52:15 +0800

    feat: Add schema.sql for notification & member_notification table.

--- share-memo-common/src/main/resources/sql/schema.sql
+++ share-memo-common/src/main/resources/sql/schema.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS member_notification;
 DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS notification;
 
 CREATE TABLE member
 (
@@ -9,4 +11,32 @@ CREATE TABLE member
     create_user      varchar(20),
     update_timestamp varchar(30),
     update_user      varchar(20)
+);
+
+CREATE TABLE notification
+(
+    id               int(6) AUTO_INCREMENT PRIMARY KEY,
+    subject          varchar(100)  NOT NULL,
+    content          varchar(4000) NOT NULL,
+    isSend           int(1),
+    create_timestamp varchar(30),
+    create_user      varchar(20),
+    update_timestamp varchar(30),
+    update_user      varchar(20)
+);
+
+CREATE TABLE member_notification
+(
+    member_id        int(6)        NOT NULL,
+    notification_id  int(6)        NOT NULL,
+    subject          varchar(100)  NOT NULL,
+    content          varchar(4000) NOT NULL,
+    isSend           char(1)       NOT NULL,
+    create_timestamp varchar(30),
+    create_user      varchar(20),
+    update_timestamp varchar(30),
+    update_user      varchar(20),
+    PRIMARY KEY (member_id, notification_id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (notification_id) REFERENCES notification (id)
 );
