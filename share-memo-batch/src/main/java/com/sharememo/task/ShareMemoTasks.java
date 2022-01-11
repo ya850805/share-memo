285933ca1c2a9335e6b73a0585366232881efad1
Jason <ya850805@gmail.com>
2022-01-11 19:37:32 +0800

    feat: Add daily morning greeting scheduler.

--- share-memo-batch/src/main/java/com/sharememo/task/ShareMemoTasks.java
+++ share-memo-batch/src/main/java/com/sharememo/task/ShareMemoTasks.java
@@ -3,35 +3,61 @@ package com.sharememo.task;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.message.TextMessage;
+import com.sharememo.entity.Member;
 import com.sharememo.entity.Notification;
 import com.sharememo.service.MemberNotificationService;
+import com.sharememo.service.MemberService;
 import com.sharememo.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /** @author Jason */
 @Slf4j
 @Component
 public class ShareMemoTasks {
+  @Autowired private MemberService memberService;
   @Autowired private NotificationService notificationService;
   @Autowired private MemberNotificationService memberNotificationService;
   @Autowired private LineMessagingClient lineMessagingClient;
 
   /** Send daily notifications at 00:00. */
-  @Scheduled(cron = "45 * * * * *")
+//  @Scheduled(cron = "45 * * * * *")
   //  @Scheduled(cron = "* * 0 * * *")
   public void sendDailyNotification() {
     log.info("Execute Daily Tasks!!!");
     List<Notification> notifications = notificationService.findByNotificationDate(LocalDate.now());
     log.info(notifications.toString());
 
-
     // TODO Send notification email.
-    lineMessagingClient.pushMessage(new PushMessage("U8fb87b06d107687799eae11f0ba0ba51", new TextMessage("test message"), true));
+    //    lineMessagingClient.pushMessage(new PushMessage("U8fb87b06d107687799eae11f0ba0ba51", new
+    // TextMessage("test message"), true));
+  }
+
+  @Scheduled(cron = "* * 9 * * *")
+  public void sendLineMorningGreeting() {
+    List<Member> members = memberService.findAllMembers();
+    members.forEach(
+        member -> {
+          if (StringUtils.isNotBlank(member.getLineId())) {
+            String userName = "";
+            try {
+              userName = lineMessagingClient.getProfile(member.getLineId()).get().getDisplayName();
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            } catch (ExecutionException e) {
+              e.printStackTrace();
+            }
+            lineMessagingClient.pushMessage(
+                new PushMessage(
+                    member.getLineId(), new TextMessage("早安！" + userName + " 今天又是美好的一天～😂"), true));
+          }
+        });
   }
 }
