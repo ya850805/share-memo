f9b3905cc01042c5b49c3065be6a6227b886b16e
Jason <ya850805@gmail.com>
2022-01-19 22:42:22 +0800

    test: Complete MemberService unit test.

--- share-memo-common/src/test/java/com/sharememo/service/MemberServiceTest.java
+++ share-memo-common/src/test/java/com/sharememo/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.sharememo.service;
 
 import com.sharememo.entity.Member;
 import com.sharememo.mapper.MemberMapper;
+import com.sharememo.mapper.MemberNotificationMapper;
 import com.sharememo.service.impl.MemberServiceImpl;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
@@ -12,11 +13,16 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class MemberServiceTest {
   @Mock private MemberMapper memberMapper;
+  @Mock private MemberNotificationService memberNotificationService;
   @InjectMocks private MemberServiceImpl memberServiceImpl;
+
 
   @Test
   public void getById_id1_NotNull() {
@@ -33,5 +39,46 @@ public class MemberServiceTest {
     memberServiceImpl.createMember(createdMember);
 
     Mockito.verify(memberMapper, Mockito.times(1)).create(createdMember);
+  }
+
+  @Test
+  public void findAllMembers_anyMembers_happenedOnce() {
+    memberServiceImpl.findAllMembers();
+
+    Mockito.verify(memberMapper, Mockito.times(1)).findAll();
+  }
+
+  @Test
+  public void deleteById_id1_happenedOnce() {
+    Integer id = 1;
+    memberServiceImpl.deleteById(id);
+
+    Mockito.verify(memberNotificationService, Mockito.times(1)).deleteByMemberId(id);
+    Mockito.verify(memberMapper, Mockito.times(1)).deleteByPrimaryKey(id);
+  }
+
+  @Test
+  public void deleteByIds_listSizeIs5_happened5Times() {
+    List<Integer> ids = Arrays.asList(1, 2, 3, 4, 5);
+    memberServiceImpl.deleteByIds(ids);
+
+    Mockito.verify(memberNotificationService, Mockito.times(5)).deleteByMemberId(Mockito.anyInt());
+    Mockito.verify(memberMapper, Mockito.times(5)).deleteByPrimaryKey(Mockito.anyInt());
+  }
+
+  @Test
+  public void deleteByLineId_anyLineId_happenedOnce() {
+    String lineId = "123456";
+    memberServiceImpl.deleteByLineId(lineId);
+
+    Mockito.verify(memberMapper, Mockito.times(1)).deleteByLineId(lineId);
+  }
+
+  @Test
+  public void updateMember_anyMember_happenedOnce() {
+    Member updatedMember = new Member();
+    memberServiceImpl.updateMember(updatedMember);
+
+    Mockito.verify(memberMapper, Mockito.times(1)).updateMember(updatedMember);
   }
 }
