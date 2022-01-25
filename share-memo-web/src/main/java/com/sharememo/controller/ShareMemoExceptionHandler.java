2408b88067cef8f5c3c3f22c8c95330bea2e47af
Jason <ya850805@gmail.com>
2022-01-25 22:10:43 +0800

    docs: Add comments for ShareMemoExceptionHandler.

--- share-memo-web/src/main/java/com/sharememo/controller/ShareMemoExceptionHandler.java
+++ share-memo-web/src/main/java/com/sharememo/controller/ShareMemoExceptionHandler.java
@@ -8,21 +8,39 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/** @author Jason */
+/**
+ * @author Jason
+ * Global exception handler.
+ */
 @Slf4j
 @RestControllerAdvice
 public class ShareMemoExceptionHandler {
+  /**
+   * Handle ShareMemoException
+   * @param e ShareMemoException
+   * @return R
+   */
   @ExceptionHandler(ShareMemoException.class)
   public R handleShareMemoException(ShareMemoException e) {
     log.error(e.getMessage());
     return new R(e.getStatusCode(), e.getMessage());
   }
 
+  /**
+   * Handle MethodArgumentNotValidException
+   * @param e MethodArgumentNotValidException
+   * @return R
+   */
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public R handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     return new R(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getFieldError().getDefaultMessage());
   }
 
+  /**
+   * Handle RuntimeException(last)
+   * @param e RuntimeException
+   * @return R
+   */
   @ExceptionHandler(RuntimeException.class)
   public R handleRuntimeException(RuntimeException e) {
     log.error(e.toString());
