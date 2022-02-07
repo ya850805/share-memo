66b9ea2b6049e0b99635491b2f1ddf5c3f0051ec
Jason <ya850805@gmail.com>
2022-02-07 21:15:42 +0800

    feat: Add test line plain text constant.

--- share-memo-common/src/main/java/com/sharememo/constant/ShareMemoConstant.java
+++ share-memo-common/src/main/java/com/sharememo/constant/ShareMemoConstant.java
@@ -12,4 +12,7 @@ public class ShareMemoConstant {
   public static final DateTimeFormatter DATE_TIME_FORMATTER =
       DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
   public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+  /*********************Line Constant**************************/
+  public static final String LINE_COMMAND = "指令";
 }
