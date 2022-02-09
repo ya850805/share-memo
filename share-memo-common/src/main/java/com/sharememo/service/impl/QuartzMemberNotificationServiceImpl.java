6757d002d76e9222dad1546d107de3e0200f98c7
Jason <ya850805@gmail.com>
2022-02-10 00:56:46 +0800

    fix: Add default column value when create QuartzMemberNotification.

--- share-memo-common/src/main/java/com/sharememo/service/impl/QuartzMemberNotificationServiceImpl.java
+++ share-memo-common/src/main/java/com/sharememo/service/impl/QuartzMemberNotificationServiceImpl.java
@@ -1,11 +1,15 @@
 package com.sharememo.service.impl;
 
+import com.sharememo.constant.ShareMemoConstant;
+import com.sharememo.constant.YNEnum;
 import com.sharememo.entity.QuartzMemberNotification;
 import com.sharememo.mapper.QuartzMemberNotificationMapper;
 import com.sharememo.service.QuartzMemberNotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 public class QuartzMemberNotificationServiceImpl implements QuartzMemberNotificationService {
@@ -14,6 +18,12 @@ public class QuartzMemberNotificationServiceImpl implements QuartzMemberNotifica
   @Override
   @Transactional
   public void create(QuartzMemberNotification quartzMemberNotification) {
+    // First time create haven't send.
+    quartzMemberNotification.setIsSend(YNEnum.N.name());
+    quartzMemberNotification.setCreateTimestamp(LocalDateTime.now());
+    quartzMemberNotification.setCreateUser(ShareMemoConstant.SYS_USER);
+    quartzMemberNotification.setUpdateTimestamp(LocalDateTime.now());
+    quartzMemberNotification.setUpdateUser(ShareMemoConstant.SYS_USER);
     quartzMemberNotificationMapper.create(quartzMemberNotification);
   }
 }
