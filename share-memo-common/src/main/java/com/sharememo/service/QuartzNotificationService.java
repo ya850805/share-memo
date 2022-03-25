8b4a9667e35f58528ab5cd77390066bd9cff52ab
Jason <ya850805@gmail.com>
2022-03-25 12:09:19 +0800

    docs: Remove unused TODO.

--- share-memo-common/src/main/java/com/sharememo/service/QuartzNotificationService.java
+++ share-memo-common/src/main/java/com/sharememo/service/QuartzNotificationService.java
@@ -7,7 +7,6 @@ import java.util.List;
 public interface QuartzNotificationService {
     void create(QuartzNotification quartzNotification, List<Integer> memberIds);
 
-    //TODO use this method.
     //Find all quartz_notification that haven't send.
     List<QuartzNotification> findAllActive();
 
