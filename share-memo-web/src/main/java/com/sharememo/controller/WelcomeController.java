729a426116fd232448793d792c762c52f4cc054f
Jason <ya850805@gmail.com>
2023-09-11 03:31:35 +0800

    feat: Add cors config

--- share-memo-web/src/main/java/com/sharememo/controller/WelcomeController.java
+++ share-memo-web/src/main/java/com/sharememo/controller/WelcomeController.java
@@ -20,10 +20,7 @@ import com.sharememo.vo.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.File;
